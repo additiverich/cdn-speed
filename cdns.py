f9ef037c20784e0e94f3061dc4182d2607d56108
David Ritchie <dritchie@gmail.com>
2015-06-02 13:54:34 +1200

    Initial commit
    Request a URL from a CDN and report on its responsiveness

---
+++ cdns.py
@@ -0,0 +1,34 @@
+import requests
+import time
+import sys
+import uuid
+
+
+urls = {
+	'microsoft': 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.4.min.js',
+	'google': 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'
+}
+
+if __name__ == "__main__":
+	url = urls[sys.argv[1]]
+	duration = int(sys.argv[2])
+
+	print "Requesting %s for %s seconds" % ( url, duration )
+
+	count = 0
+	time_so_far = 0
+	start_time = time.time()
+	while True:
+		time_so_far = time.time() - start_time
+
+		if(time_so_far >= duration):
+			break
+
+		u = "%s?rand=%s" % ( url, str(uuid.uuid4().get_hex().upper()[0:6]))
+		js = requests.get(u)
+		count += 1
+
+		# if(count % 5 == 0):
+			# print "Requested %s times" % count
+
+	print "Requested %s times (%ss per request)" % ( count, time_so_far / count )
